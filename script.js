61366aa42662078fdd6869c93de39c9e2d8d4d6d
Peppecoding <83593668+Peppecoding@users.noreply.github.com>
2024-05-08 09:34:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function changePreview(url) {
     document.getElementById('previewFrame').src = url;
 }
 
+function changePreviewFromInput() {
+    var url = document.getElementById('urlInput').value;
+    if (url) {
+        document.getElementById('previewFrame').src = url;
+    } else {
+        alert('Please enter a valid URL.');
+    }
+}
